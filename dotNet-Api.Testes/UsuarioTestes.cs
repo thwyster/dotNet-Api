5d51955d2180988c2fea5478e76d56e2345476c3
Ronny Czerkus <ronny.czerkus@inlog.com>
2019-06-28 18:56:31 -0300

    Implementando alguns testes

---
+++ dotNet-Api.Testes/UsuarioTestes.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace dotNet_Api
+{
+    public class UsuarioTestes
+    {
+        [Fact]
+        public void TesteUm()
+        {
+            throw new NotImplementedException("Implemente algum teste antes");
+        }
+
+        [Fact]
+        public void TesteDois()
+        {
+            throw new NotImplementedException("Implemente outro teste");
+        }
+    }
+}
